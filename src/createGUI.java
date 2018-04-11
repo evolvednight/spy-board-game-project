5820cfc47cf5024c3e14ee5c3a675dc22e63775b
austinvandood <austinvandood@10.84.73.111>
2018-04-11 15:16:01 -0400

    //

--- src/createGUI.java
+++ src/createGUI.java
@@ -34,385 +34,309 @@ public class createGUI implements Observer {
 	private JFrame aliasx2;
 	private int clueCount;
 	board ssr = new board();
-	int xx =ssr.getTurn() ;
+	int xx = ssr.getTurn();
+
 	public createGUI() {
 		restart();
 	}
+
 	public void restart() {
 		MakeGui();
 	}
-	
-	
+
 	public void MakeGui() {
-		
+
 		main m = new main();
 		m.gameStart();
 
-		
-		
-		if(m.winningState() == true) {
-			JFrame win= new JFrame("Game Won");
+		if (m.winningState() == true) {
+			JFrame win = new JFrame("Game Won");
 			win.setLocation(450, 250);
-			
-			if(m.getNumRed() == 0) {
+
+			if (m.getNumRed() == 0) {
 				ImageIcon icon = new ImageIcon("src/red.jpg");
 				JLabel label = new JLabel(icon);
-				
+
 				win.add(label);
-				  win.setDefaultCloseOperation
-				         (JFrame.DISPOSE_ON_CLOSE);
-				  win.pack();
-			}
-			else {
+				win.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				win.pack();
+			} else {
 				ImageIcon icon = new ImageIcon("src/blue.jpg");
 				JLabel label = new JLabel(icon);
-				
+
 				win.add(label);
-				  win.setDefaultCloseOperation
-				         (JFrame.DISPOSE_ON_CLOSE);
-				  win.pack();
+				win.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				win.pack();
 			}
 		}
 
-	
-
 		JMenuBar menuBar = new JMenuBar();
-		JMenu menu = new JMenu("File"); 
+		JMenu menu = new JMenu("File");
 		JMenuItem start = new JMenuItem("Start New Game");
 		JMenuItem exit = new JMenuItem("Exit Game");
 		menuBar.add(menu);
 		menu.add(start);
 		menu.add(exit);
-		
-		
+
 		JMenuBar menuBar2 = new JMenuBar();
-		JMenu menu2 = new JMenu("File"); 
+		JMenu menu2 = new JMenu("File");
 		JMenuItem exit2 = new JMenuItem("Exit Spymaster");
 		menuBar2.add(menu2);
 		menu2.add(exit2);
-		
-		
-		
+
 		JFrame x1 = new JFrame("CodeName game");
 		x1.setJMenuBar(menuBar);
 		x1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	//	x1.setSize(1000,1000);
-		x1.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		// x1.setSize(1000,1000);
+		x1.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		x1.setUndecorated(true);
 		x1.setVisible(true);
 
-		GridLayout inX = new GridLayout(2,2);
+		GridLayout inX = new GridLayout(2, 2);
 		x1.setLayout(inX);
-		JPanel panelTop = new JPanel(new GridLayout(6,6));
-		JPanel panelBottom = new JPanel(new GridLayout(5,5));
+		JPanel panelTop = new JPanel(new GridLayout(6, 6));
+		JPanel panelBottom = new JPanel(new GridLayout(5, 5));
 		JLabel labelCode = new JLabel();
 		JLabel labelNumber = new JLabel();
 		labelCode.setText("Hint: ");
 		labelNumber.setText("Number: ");
 		labelCode.setFont(new Font("Serif", Font.PLAIN, 40));
 		labelNumber.setFont(new Font("Serif", Font.PLAIN, 40));
-		
-		
-		
-		
-		ActionListener exit3 = new ActionListener() {//menu exit
+
+		ActionListener exit3 = new ActionListener() {// menu exit
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				x1.dispose();
-				aliasx2.dispose();	
+				aliasx2.dispose();
 			}
-		}
-		;
+		};
 		exit.addActionListener(exit3);
-		
-		
-		ActionListener exit4 = new ActionListener() {//menu exit
+
+		ActionListener exit4 = new ActionListener() {// menu exit
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				aliasx2.dispose();	
+				aliasx2.dispose();
 			}
-		}
-		;
+		};
 		exit2.addActionListener(exit4);
-		
-		
-		
-		
-		ActionListener start2 = new ActionListener() {//menu restart
+
+		ActionListener start2 = new ActionListener() {// menu restart
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				x1.getContentPane().removeAll();
 				x1.dispose();
 				restart();
 			}
-		}
-		;
+		};
 		start.addActionListener(start2);
-		
-		
-		
-		
-		
-		
-		
-		
+
 		JButton restart = new JButton("Restart");
-		
+
 		restart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
 				if (e.getSource() == restart) {
 					x1.getContentPane().removeAll();
 					x1.dispose();
-				restart();
+					restart();
 
 				}
 			}
 		});
 
 		panelTop.add(restart);
-		
-		
 
-		
-		JButton Spy = new JButton( "Spy Master");
+		JButton Spy = new JButton("Spy Master");
 		Spy.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				JFrame x2 = new JFrame("Spy Master Window");
 				aliasx2 = x2;
 				JPanel panelRight = new JPanel();
-//				panelRight.setLayout(new GridLayout(0,1));
+				// panelRight.setLayout(new GridLayout(0,1));
 				JPanel panelLeft = new JPanel();
-				panelLeft.setLayout(new GridLayout(5,5));
+				panelLeft.setLayout(new GridLayout(5, 5));
 				JLabel label1 = new JLabel();
 				JLabel label2 = new JLabel();
 				JButton jb = new JButton("Enter");
-				
-				
+
 				System.out.println(m.getNumRed());
-				
-				JTextField textField= new JTextField("Input Clue" ,20);
+
+				JTextField textField = new JTextField("Input Clue", 20);
 				JTextField numField = new JTextField("Input number", 20);
-				if(textField.getText().equals("Input Clue")) {
-				textField.setForeground(Color.LIGHT_GRAY);
+				if (textField.getText().equals("Input Clue")) {
+					textField.setForeground(Color.LIGHT_GRAY);
 				}
-				if(numField.getText().equals("Input number")) {
-				numField.setForeground(Color.LIGHT_GRAY);
+				if (numField.getText().equals("Input number")) {
+					numField.setForeground(Color.LIGHT_GRAY);
 				}
-				
-				
-				
-				
-				FocusListener ghost1 = new FocusListener(){
+
+				FocusListener ghost1 = new FocusListener() {
 					@Override
 					public void focusGained(FocusEvent e) {
 						textField.setText("");
 						textField.setForeground(Color.black);
 					}
+
 					@Override
 					public void focusLost(FocusEvent e) {
-						if(textField.getText().equals("")) {
+						if (textField.getText().equals("")) {
 							textField.setText("Input Clue");
 							textField.setForeground(Color.LIGHT_GRAY);
 						}
-					 }	
-				    }
-				;
-				
-				
-				
-				
-				
-				FocusListener ghost2 = new FocusListener(){
+					}
+				};
+
+				FocusListener ghost2 = new FocusListener() {
 					@Override
 					public void focusGained(FocusEvent e) {
 						numField.setText("");
 						numField.setForeground(Color.black);
 					}
+
 					@Override
 					public void focusLost(FocusEvent e) {
-						if(numField.getText().equals("")) {
+						if (numField.getText().equals("")) {
 							numField.setText("Input number");
 							numField.setForeground(Color.LIGHT_GRAY);
 						}
-					 }	
-				    }
-				;
-			textField.addFocusListener(ghost1);
-			numField.addFocusListener(ghost2);
-				
-				
-				
-				
-				
+					}
+				};
+				textField.addFocusListener(ghost1);
+				numField.addFocusListener(ghost2);
+
 				jb.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						String input = textField.getText();
 						int x = Integer.parseInt(numField.getText());
 						clueCount = x;
 						x2.dispose();
-						if ( m.Legality(input) == true && m.legalityNum(x)) {
+						if (m.Legality(input) == true && m.legalityNum(x)) {
 
 							label1.setText(input);
 							label2.setText(numField.getText());
-							labelCode.setText("Hint: "+ input);
-							labelNumber.setText("Number: "+ numField.getText());
+							labelCode.setText("Hint: " + input);
+							labelNumber.setText("Number: " + clueCount);
 							labelCode.setFont(new Font("Serif", Font.PLAIN, 40));
 							labelNumber.setFont(new Font("Serif", Font.PLAIN, 40));
-							
-							
-							if(input.equalsIgnoreCase("hertz")) {
+
+							if (input.equalsIgnoreCase("hertz")) {
 								JFrame frame = new JFrame("EasterEgg");
 								frame.setLocation(450, 250);
 								ImageIcon icon = new ImageIcon("src/hertz.jpg");
 								JLabel label = new JLabel(icon);
-								
+
 								frame.add(label);
-								  frame.setDefaultCloseOperation
-								         (JFrame.DISPOSE_ON_CLOSE);
-								  frame.pack();
-								  
-								  frame.setVisible(true);
+								frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+								frame.pack();
+
+								frame.setVisible(true);
 							}
-							
-							if(input.equalsIgnoreCase("AustinsJeep")) {
-							JFrame f = new JFrame(); 
 
+							if (input.equalsIgnoreCase("testingegg")) {
+								JFrame f = new JFrame();
 
-					        ImageIcon image = new ImageIcon("src/img.jpg"); //imports the image
+								ImageIcon image = new ImageIcon("src/img.jpg"); // imports the image
 
-					        JLabel lbl = new JLabel(image); //puts the image into a jlabel
+								JLabel lbl = new JLabel(image); // puts the image into a jlabel
 
-					        f.getContentPane().add(lbl); //puts label inside the jframe
+								f.getContentPane().add(lbl); // puts label inside the jframe
 
-					        f.setSize(image.getIconWidth(), image.getIconHeight()); //gets h and w of image and sets jframe to the size
-					        
+								f.setSize(image.getIconWidth(), image.getIconHeight()); // gets h and w of image and
+																						// sets jframe to the size
 
-					 
-					        f.setLocation(150, 250); //sets the location of the jframe
-					        f.setVisible(true); //makes the jframe visible
+								f.setLocation(150, 250); // sets the location of the jframe
+								f.setVisible(true); // makes the jframe visible
 							}
-					    }
-					
-				
-						
-						else if(m.Legality(input) && !m.legalityNum(x)) {
+						}
+
+						else if (m.Legality(input) && !m.legalityNum(x)) {
+							label1.setText("Invalid code or number");
+						} else if (!m.Legality(input) && m.legalityNum(x)) {
+							label1.setText("Invalid code or number");
+						} else {
 							label1.setText("Invalid code or number");
 						}
-						else if( !m.Legality(input) && m.legalityNum(x)) {
-							label1.setText("Invalid code or number");
-						}
-						else {
-							label1.setText("Invalid code or number");
-						}
-						
+
 					}
-					
+
 				});
-				
-				
-				
-				
-				
-				
-				
-//				notifyObserver();
+
+				// notifyObserver();
 				panelRight.add(jb);
 				panelRight.add(textField);
 				panelRight.add(numField);
 				panelRight.add(label1);
 				panelRight.add(label2);
-				x2.getContentPane().add(panelRight,BorderLayout.EAST);
-				
-				
-				
+				x2.getContentPane().add(panelRight, BorderLayout.EAST);
+
 				x2.setJMenuBar(menuBar2);
-				//x2.setSize(750, 750);
-			
-				GridLayout inY = new GridLayout(0,2);
+				// x2.setSize(750, 750);
+
+				GridLayout inY = new GridLayout(0, 2);
 				x2.setLayout(inY);
-				for( int i = 0; i<25 ; i++) {
+				for (int i = 0; i < 25; i++) {
 					String temp = m.allLocations.get(i).getCodeName();
 					JButton j = new JButton(temp);
-					if(m.allLocations.get(i).getTeam() == "Red") {
+					if (m.allLocations.get(i).getTeam() == "Red") {
 						j.setBackground(Color.RED);
-	
-					}
-					else if( m.allLocations.get(i).getTeam() == "Blue") {
+
+					} else if (m.allLocations.get(i).getTeam() == "Blue") {
 						j.setBackground(Color.BLUE);
-						
-					}
-					else if ( m.allLocations.get(i).getTeam() == "Bystander") {
+
+					} else if (m.allLocations.get(i).getTeam() == "Bystander") {
 						j.setBackground(Color.YELLOW);
-				
-					}
-					else if ( m.allLocations.get(i).getTeam() == "Assassin") {
+
+					} else if (m.allLocations.get(i).getTeam() == "Assassin") {
 						j.setBackground(Color.GREEN);
-						
+
 					}
 					panelLeft.add(j);
 					x2.add(panelLeft);
 
 				}
-				x2.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+				x2.setExtendedState(JFrame.MAXIMIZED_BOTH);
 				x2.setUndecorated(true);
 				x2.setVisible(true);
-			
+
 				x2.setVisible(true);
 			}
-		}
-			);
-		
-		
-		
-		
-		
+		});
+
 		panelTop.add(Spy);
 		panelTop.add(labelCode);
 		panelTop.add(labelNumber);
-		
-		
-		
-		
-		
 
-		JButton tend = new JButton("End turn") ;// lets make this the the button that ends turns
+		JButton tend = new JButton("End turn");// lets make this the the button that ends turns
 		panelTop.add(tend);
 		tend.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent t) {
 				ssr.changeTurn();
 				labelCode.setText("Hint: ");
 				labelNumber.setText("Number: ");
-				//labelCode.setFont(new Font("Serif", Font.PLAIN, 40));
-				//labelNumber.setFont(new Font("Serif", Font.PLAIN, 40));
+				// labelCode.setFont(new Font("Serif", Font.PLAIN, 40));
+				// labelNumber.setFont(new Font("Serif", Font.PLAIN, 40));
 
-				
 			}
 		});
-		
-		
-		
-		
-		
-		
 
 		JLabel disturn = new JLabel();
-		if(xx == 1) {disturn.setText("Turn : Team Red");
-		disturn.setForeground(Color.red);
-		disturn.setFont(new Font("Serif", Font.PLAIN, 40));}
-		
-		if(xx == 0) {disturn.setText("Turn : Team Blue");
-		disturn.setForeground(Color.blue);
-		disturn.setFont(new Font("Serif", Font.PLAIN, 40));
-		
+		if (xx == 1) {
+			disturn.setText("Turn : Team Red");
+			disturn.setForeground(Color.red);
+			disturn.setFont(new Font("Serif", Font.PLAIN, 40));
+		}
+
+		if (xx == 0) {
+			disturn.setText("Turn : Team Blue");
+			disturn.setForeground(Color.blue);
+			disturn.setFont(new Font("Serif", Font.PLAIN, 40));
+
 		}
 		panelTop.add(disturn);
 
-		
-		
-		//instruction here
-		
+		// instruction here
+
 		JLabel redScore = new JLabel("Red Team Points: " + m.getNumRed());
 		redScore.setForeground(Color.red);
 		redScore.setFont(new Font("Serif", Font.PLAIN, 40));
@@ -423,74 +347,71 @@ public class createGUI implements Observer {
 		blueScore.setFont(new Font("Serif", Font.PLAIN, 40));
 		panelTop.add(blueScore);
 
-//		JTextArea countField = new JTextArea();
-//		countField.setText("this field is for red and blue counter");
-//		panelTop.add(countField); 
+		// JTextArea countField = new JTextArea();
+		// countField.setText("this field is for red and blue counter");
+		// panelTop.add(countField);
 
-	/*	//hasan
-		JTextArea clueAndCount = new JTextArea() ;
-		String x = "set equal to clue getter, " ;//need getters
-		String g = "set equal to number of cards the clue applies to" ;//need getters
-		clueAndCount.setText(x + g);
-		panelTop.add(clueAndCount);
-		//end 
-*/
+		/*
+		 * //hasan JTextArea clueAndCount = new JTextArea() ; String x =
+		 * "set equal to clue getter, " ;//need getters String g =
+		 * "set equal to number of cards the clue applies to" ;//need getters
+		 * clueAndCount.setText(x + g); panelTop.add(clueAndCount); //end
+		 */
 
-		
-		
-	//hh	
-		
-	
-		
-		for( int i = 0; i<25 ; i++) {
+		// hh
+
+		for (int i = 0; i < 25; i++) {
 			String temp = m.allLocations.get(i).getCodeName();
 			JButton j = new JButton(temp);
-			String team = m.allLocations.get(i).getTeam() ;
-			
-			
-		panelBottom.add(j);
-		j.addActionListener(new ActionListener( ) {
-			public void actionPerformed(ActionEvent E) {
-				boolean turnOver = false ;
-				clueCount = clueCount - 1 ;
-				if(clueCount == 0) { 
-					ssr.changeTurn(); 
-				 turnOver = true ;
+			String team = m.allLocations.get(i).getTeam();
+
+			panelBottom.add(j);
+			j.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent E) {
+					boolean turnOver = false;
+					clueCount = clueCount - 1;
+					if (clueCount == 0) {
+						ssr.changeTurn();
+						turnOver = true;
+
+					}
+
+					if (team == "Red") {
+						if (xx != 1 && turnOver == false) {
+							ssr.changeTurn();
+							clueCount = 0;
+						}
+						j.setForeground(Color.red);
+					} else if (team == "Blue") {
+						if (xx != 0 && turnOver == false) {
+							ssr.changeTurn();
+							clueCount = 0;
+						}
+						j.setForeground(Color.blue);
+
+					} else if (team == "Bystander") {
+						j.setForeground(Color.yellow);
+						if (turnOver == false) {
+							ssr.changeTurn();
+							clueCount = 0;
+						}
+
+					} else if (team == "Assassin") {
+						j.setForeground(Color.green);
+					}
 				}
-				
-				if(team == "Red") {
-					if(xx != 1 && turnOver == false) { ssr.changeTurn();clueCount = 0 ;}
-					j.setForeground(Color.red);
-				}
-				else if(team == "Blue") {
-					if(xx != 0 && turnOver == false) { ssr.changeTurn(); clueCount = 0 ;}
-					j.setForeground(Color.blue);
-					
-				}
-				else if ( team == "Bystander") {
-					j.setForeground(Color.yellow);
-				if(turnOver == false) {	ssr.changeTurn(); clueCount = 0 ;}
-					
-				}
-				else if ( team == "Assassin") {
-					j.setForeground(Color.green);
-				}
-			}
-		});
-		
-		
-		//ll
+			});
+
+			// ll
 		}
 		x1.add(panelTop);
 		x1.add(panelBottom);
 		x1.setVisible(true);
 	}
-	
-	
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
-		
-	}		
+
+	}
 }
